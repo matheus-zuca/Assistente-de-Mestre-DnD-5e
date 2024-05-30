a4184d53af9b2253b06b7391b27829808e583b9d
Matheus Zuca <matheusvzucca@gmail.com>
2024-05-30 04:07:49 -0300

    feat: Alterado o funcionamento de toda a preparação de magias

--- commands/prepararmagia.js
+++ commands/prepararmagia.js
@@ -5,7 +5,41 @@ const functions = require('../modules/functions.js');
 
 module.exports.run = async (bot, message, comando, personagemDoJogador) => {
 
-    lvl_prep_magia = 0;
+    var magia = comando.slice(1, comando.lenght).join(" ");
+
+    if(magia == ""){
+        message.channel.send(personagemDoJogador.Magias_Preparadas.sort().reduce((acc, curr) => {
+            acc += curr + "\n";
+            return acc;
+        }, ""))
+
+        return;
+    }
+
+    for(var classe in variables.Classes_Prep_Magia){
+        if(personagemDoJogador.Level[variables.Classes_Prep_Magia[classe]] == 0){
+            continue;
+        }
+        magias_conhecidas = variables.rel_class_magias()[variables.Classes_Prep_Magia[classe]][personagemDoJogador.Level[variables.Classes_Prep_Magia[classe]]]
+        for(var key in magias_conhecidas){
+            if(magias_conhecidas[key] > 0){
+                lvl_max_permitido = key;
+            }
+        }
+
+        if(variables.spells[magia].classes.includes(variables.Classes_Prep_Magia[classe]) && variables.spells[magia].Nivel <= lvl_max_permitido){
+            console.log(variables.spells[magia].classes.includes(variables.Classes_Prep_Magia[classe]))
+            console.log(variables.Classes_Prep_Magia[classe])
+            personagemDoJogador.Magias_Preparadas.push(magia)
+            message.channel.send("Magia Preparada com Sucesso")
+            return;
+        }
+    }
+
+    message.channel.send("Não foi possível encontrar a magia em questão")
+
+
+    /*lvl_prep_magia = 0;
     text_prep_magia = ""
     total_magias_prep = 0;
     variables.Classes_Prep_Magia.forEach(className => {
@@ -34,7 +68,7 @@ module.exports.run = async (bot, message, comando, personagemDoJogador) => {
     habilidadeConjuradora = variables.rel_class_habConju().getKeyByValue(resposta);
     magiasPreparadas = variables.qtde_magias_preparadas(personagemDoJogador[habilidadeConjuradora], personagemDoJogador.Level[resposta]);
 
-    message.channel.send(`Você pode preparar ${magiasPreparadas} magias de ${resposta}\n Você pode escolher as seguintes magias:`);
+    message.channel.send(`Você pode preparar ${magiasPreparadas} magias de ${resposta}\n`);
     magias = variables.rel_class_magias()[resposta][personagemDoJogador.Level[resposta]]
     lvl_magias = {}
     lvl_max_permitido = 0;
@@ -46,9 +80,19 @@ module.exports.run = async (bot, message, comando, personagemDoJogador) => {
         }
     }
 
-    for(var magia in variables.spells){
-        
+    todas_magias = [];
+    for(var i = lvl_max_permitido; i>=0; i--){
+        for(var magia in variables.spells){
+            if(variables.spells[magia].Nivel == i && variables.spells[magia].classes.includes(resposta)){
+                todas_magias.push(`${magia} - Magia de ${i}º Ciclo`)
+            }
+        }  
     }
+
+    EmbedMagia = new Discord.MessageEmbed().setTitle("Você pode escolher as seguintes magias:");
+    EmbedMagia.setDescription(todas_magias);
+
+    message.channel.send(EmbedMagia);*/
 
 }
 
