3aa84f34ca4dc6d9a2b8dc7fd18aa5bcad28f4ef
Matheus Zuca <matheusvzucca@gmail.com>
2024-06-08 19:55:29 -0300

    refactor: Alteração no código da consulta de ficha

--- commands/consultaFicha.js
+++ commands/consultaFicha.js
@@ -7,7 +7,35 @@ module.exports.run = async (bot, message, comando, personagemDoJogador) => {
 		message.channel.send("Não foi possível encontrar sua ficha, mas você pode faze-la usando o nosso bot")
 		return;
 	}
+	
+    var ficha_embed = new Discord.MessageEmbed()
+					.setTitle("Ficha de Personagem")
+					.addFields(
+						{ name: 'Nome:', value: personagemDoJogador.Nome, inline: true },
+						{ name: "Raça", value: personagemDoJogador.Raça, inline: true },
+						{ name: "Classe e Nivel", value: functions.GetClass(personagemDoJogador), inline: true },
+						{ name: "Tendência", value: "Alguma", inline: true },
+						{ name: "HP", value: personagemDoJogador.HP + " (" + personagemDoJogador.HPMax + ")", inline: true },
+						{ name: "CA", value: personagemDoJogador.CA, inline: true }
+					)
+					.addField("\u200B", "\u200B")
+					.addFields(
+						{ name: "Força", value: personagemDoJogador.HabilidadeForça + " (" + personagemDoJogador.Força + ")", inline: true },
+						{ name: "Destreza", value: personagemDoJogador.HabilidadeDestreza + " (" + personagemDoJogador.Destreza + ")", inline: true },
+						{ name: "Constituição", value: personagemDoJogador.HabilidadeConstituição + " (" + personagemDoJogador.Constituição + ")", inline: true },
+						{ name: "Inteligência", value: personagemDoJogador.HabilidadeInteligência + " (" + personagemDoJogador.Inteligência + ")", inline: true },
+						{ name: "Sabedoria", value: personagemDoJogador.HabilidadeSabedoria + " (" + personagemDoJogador.Sabedoria + ")", inline: true },
+						{ name: "Carisma", value: personagemDoJogador.HabilidadeCarisma + " (" + personagemDoJogador.Carisma + ")", inline: true }
+					)
+					.addField("Proficiências", "```" + personagemDoJogador.Proficiencias.join(", ") + "```")
+					.addField("Valor da Proficiência", personagemDoJogador.ValorProficiencia)
+					.addField("Armas", "```" + GetArrayArmas() + "```")
+					.addField("Arma Atual", personagemDoJogador.ArmaSelecionada);
+	
+	message.channel.send(ficha_embed);
+}
 
+function GetArrayArmas(){
 	var armas = ""
 
 	ArmasArray = personagemDoJogador.Armas.sort().reduce(function (acc, curr) {
@@ -38,31 +66,7 @@ module.exports.run = async (bot, message, comando, personagemDoJogador) => {
 		
     });
 
-    var ficha_embed = new Discord.MessageEmbed()
-					.setTitle("Ficha de Personagem")
-					.addFields(
-						{ name: 'Nome:', value: personagemDoJogador.Nome, inline: true },
-						{ name: "Raça", value: personagemDoJogador.Raça, inline: true },
-						{ name: "Classe e Nivel", value: functions.GetClass(personagemDoJogador), inline: true },
-						{ name: "Tendência", value: "Alguma", inline: true },
-						{ name: "HP", value: personagemDoJogador.HP + " (" + personagemDoJogador.HPMax + ")", inline: true },
-						{ name: "CA", value: personagemDoJogador.CA, inline: true }
-					)
-					.addField("\u200B", "\u200B")
-					.addFields(
-						{ name: "Força", value: personagemDoJogador.HabilidadeForça + " (" + personagemDoJogador.Força + ")", inline: true },
-						{ name: "Destreza", value: personagemDoJogador.HabilidadeDestreza + " (" + personagemDoJogador.Destreza + ")", inline: true },
-						{ name: "Constituição", value: personagemDoJogador.HabilidadeConstituição + " (" + personagemDoJogador.Constituição + ")", inline: true },
-						{ name: "Inteligência", value: personagemDoJogador.HabilidadeInteligência + " (" + personagemDoJogador.Inteligência + ")", inline: true },
-						{ name: "Sabedoria", value: personagemDoJogador.HabilidadeSabedoria + " (" + personagemDoJogador.Sabedoria + ")", inline: true },
-						{ name: "Carisma", value: personagemDoJogador.HabilidadeCarisma + " (" + personagemDoJogador.Carisma + ")", inline: true }
-					)
-					.addField("Proficiências", "```" + personagemDoJogador.Proficiencias.join(", ") + "```")
-					.addField("Valor da Proficiência", personagemDoJogador.ValorProficiencia)
-					.addField("Armas", "```" + armas + "```")
-					.addField("Arma Atual", personagemDoJogador.ArmaSelecionada);
-	
-	message.channel.send(ficha_embed);
+	return armas;
 }
 
 module.exports.help = {
