640d413f5d82ceed75ecc26e4690217253407201
Matheus Zuca <matheusvzucca@gmail.com>
2024-06-08 19:44:52 -0300

    refactor: Alteração estetica no envio de anotações

--- commands/anotacoes.js
+++ commands/anotacoes.js
@@ -3,7 +3,10 @@ const functions = require("../modules/functions.js");
 const variables = require("../modules/variables.js");
 
 module.exports.run = async (bot, message, comando, personagemDoJogador) => {
-    message.author.send(personagemDoJogador.Anotacoes);
+    anotacoes = new Discord.MessageEmbed();
+    anotacoes.setTitle("Suas anotações")
+    anotacoes.setDescription(personagemDoJogador.Anotacoes)
+    message.author.send(anotacoes);
     message.channel.send("Suas anotações foram enviadas!");
 }
 
