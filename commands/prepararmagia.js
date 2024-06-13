ee556343aa5de66b189fb7cf23fb96f89220d16b
Matheus Zuca <matheusvzucca@gmail.com>
2024-06-13 18:04:41 -0300

    refactor: Retirado código não-usado

--- commands/prepararmagia.js
+++ commands/prepararmagia.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const prefix = require('../auth.json');
 const variables = require('../modules/variables.js');
 const functions = require('../modules/functions.js');
 
